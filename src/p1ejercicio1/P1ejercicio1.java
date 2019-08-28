05a87ed7633dbd7c917ad3df63e355105be3a256
babolat22 <juanj@DESKTOP-T4DG6TR>
2019-08-28 18:23:52 -0300

    Version con fechas y period

--- src/p1ejercicio1/P1ejercicio1.java
+++ src/p1ejercicio1/P1ejercicio1.java
@@ -8,6 +8,7 @@ package p1ejercicio1;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
+
 /**
  *
  * @author juanj
@@ -18,18 +19,24 @@ public class P1ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n1, n2, n3;
+        int i=1, n1=0, n2, n3;
         System.out.println("Ingrese 3 numeros:");
+        i = JOptionPane.showConfirmDialog(null,"¿Realmente Desea Salir de Hola Swing?","Confirmar Salida",JOptionPane.YES_NO_OPTION);
+        if(i==0){
+            System.exit(0);
+         }
         n1= Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese: "));  // con dialogo
         Scanner sc = new Scanner(System.in);     // por consola
-     // n1 = sc.nextInt();
+       // n1 = sc.nextInt();
         n2 = sc.nextInt();
         n3 = sc.nextInt();
-        if (n1<n2 && n3<n2) 
+
+        if (n1<n2 && n3<n2) {
             System.out.println("El matyor es: "+ n2);
-        else
+           
+        }else
             if(n2<n1 && n3<n1)
-                    System.out.println("El matyor es: "+ n1);
+                System.out.println("El matyor es: "+ n1);
             else
                 System.out.println("El mayor es: "+ n3);
         System.out.println("JAJA GANE ");
